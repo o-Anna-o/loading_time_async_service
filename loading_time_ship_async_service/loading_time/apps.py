a58c90dd21affcb137efbe62f795edd42b1df8ba
o-Anna-o <annashulzhinskaya26@gmail.com>
2025-12-21 15:54:35 +0300

    чистое приложение, без настроек

--- loading_time_ship_async_service/loading_time/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LoadingTimeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'loading_time'
--- loading_time_ship_async_service/loading_time_ship_async_service/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from loading_time import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.set_status, name='set-status'),
-]
